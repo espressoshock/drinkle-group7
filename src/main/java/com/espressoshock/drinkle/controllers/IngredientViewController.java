ab60bb2f85fe5e3228e0e1412c1cfff08dcf9b74
Alex Oachesu <alex.oachesu0001@stud.hkr.se>
2019-05-02 16:41:04 +0200

    ingredient view search option
    edit search option

--- src/main/java/com/espressoshock/drinkle/controllers/IngredientViewController.java
+++ src/main/java/com/espressoshock/drinkle/controllers/IngredientViewController.java
@@ -32,7 +32,7 @@ public class IngredientViewController implements Initializable {
             button21, button22,button23,button24,button25,button26,button27,button28,button29,button30,
             button31, button32,button33,button34,button35,button36,button37,button38,button39,button40,
             button41, button42,button43,button44,button45,button46,button47,button48, btnCreate,
-            btnAddToInventory, btnInvisibleOne, btnInvisibleTwo, btnInvisibleThree;
+            btnAddToInventory, btnInvisibleOne, btnInvisibleTwo, btnInvisibleThree, btnSearch;
     @FXML
     private AnchorPane anchorPaneIngredients;
     @FXML
