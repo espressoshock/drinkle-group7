b84dc890f631f1bb046a34ebe4ea53e63549e519
Sergheev Andrian <sergheev@icloud.com>
2019-05-15 20:25:43 +0200

    removed hibernate

--- src/main/java/com/espressoshock/drinkle/daoLayer/entities/AccountDalDao.java
+++
@@ -1,6 +0,0 @@
-package com.espressoshock.drinkle.daoLayer.entities;
-
-import com.espressoshock.drinkle.daoLayer.IDao;
-
-public interface AccountDalDao extends IDao<String, AccountDAL> {
-}
--- src/main/java/com/espressoshock/drinkle/daoLayer/entities/BusinessAccountDao.java
+++
@@ -1,7 +0,0 @@
-package com.espressoshock.drinkle.daoLayer.entities;
-
-import com.espressoshock.drinkle.daoLayer.IDao;
-import com.espressoshock.drinkle.models.BusinessAccount;
-
-public interface BusinessAccountDao extends IDao<String, BusinessAccount> {
-}
